7f7d8ae3b22bc6d58465a98a683f560a259cedf8
Jarek Lisik <jaroek12@gmail.com>
2021-05-22 15:43:22 +0200

    update ursl

--- new_project/maths/urls.py
+++ new_project/maths/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<number1>/<number2>/', views.add),
-    path('<number1>/<number2>/', views.sub),
-    path('<number1>/<number2>/', views.div),
-    path('<number1>/<number2>/', views.mul),
+    path('add/<number1>/<number2>/', views.add),
+    path('sub/<number1>/<number2>/', views.sub),
+    path('div/<number1>/<number2>/', views.div),
+    path('mul/<number1>/<number2>/', views.mul),
 ]
