b9ad1a3010f5eb1abbfb22d972bf9ac4304c6b14
Jarek Lisik <jaroek12@gmail.com>
2021-05-22 16:12:57 +0200

    dzielenie przez 0

--- new_project/maths/views.py
+++ new_project/maths/views.py
@@ -13,7 +13,7 @@ def sub(request, number1, number2):
 
 
 def div(request, number1, number2):
-    if number2 == 0:
+    if number2 == "0":
         return HttpResponse("nie dziel przez 0")
     return HttpResponse(int(number1) / int(number2))
 
