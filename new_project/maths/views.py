4052c77914f400d1bfbfa3940eaab3be061b40cc
PatAug <pataug91@gmail.com>
2021-05-22 15:56:30 +0200

    wyjatek dzielenie

--- new_project/maths/views.py
+++ new_project/maths/views.py
@@ -13,6 +13,8 @@ def sub(request, number1, number2):
 
 
 def div(request, number1, number2):
+    if number2 == 0:
+        return HttpResponse("nie dziel przez 0")
     return HttpResponse(int(number1) / int(number2))
 
 
